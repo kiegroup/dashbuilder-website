5cd942d5594b35a2ae714602d04c80550191581b
William Siqueira <william.fatecsjc@gmail.com>
2022-06-01 17:22:28 -0300

    Updates

---
+++ dashbuilder-website-assembly/src/dashbuilder/setup.js
@@ -0,0 +1,6 @@
+dashbuilder = {
+  hideNavBar: true,
+  dashboards: [
+    "https://raw.githubusercontent.com/kiegroup/kie-tools/main/examples/dashbuilder/kitchensink/Kitchensink.dash.yml",
+  ],
+};
