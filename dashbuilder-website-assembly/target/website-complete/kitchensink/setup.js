47eb71ca9da5a6935ecd22f69965ca60ea0daa38
William Siqueira <william.fatecsjc@gmail.com>
2023-08-23 17:08:32 -0300

    Update

--- dashbuilder-website-assembly/target/website-complete/kitchensink/setup.js
+++
@@ -1,9 +0,0 @@
-dashbuilder = {
-  hideNavBar: true,
-  allowExternal: true,
-  samplesUrl:
-    "https://raw.githubusercontent.com/kiegroup/kie-samples/main/samples/",
-  dashboards: [
-    "https://raw.githubusercontent.com/kiegroup/kie-samples/main/samples/dashbuilder-kitchensink/dashbuilder-kitchensink.dash.yaml",
-  ],
-};
